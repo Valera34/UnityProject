07be137d1f1509da91ef3b0ef21e17d78605bf8b
Valera34 <Valeraanikeenko@gmail.com>
2017-05-20 15:54:28 +0300

    Second task

---
+++ Assets/Content/Scripts/DeathHere.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathHere : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        
+        HeroController rabit = collider.GetComponent<HeroController>();
+        
+        if (rabit != null)
+        {
+            LevelController.current.onRabitDeath(rabit);
+        }
+    }
+}
--- Assets/Content/Scripts/HeroController.cs
+++ Assets/Content/Scripts/HeroController.cs
@@ -4,17 +4,41 @@ using UnityEngine;
 
 public class HeroController : MonoBehaviour {
     public float speed = 3;
+    bool isGrounded = false;
+    bool JumpActive = false;
+    float JumpTime = 0f;
+    public float MaxJumpTime = 2f;
+    public float JumpSpeed = 2f;
     Rigidbody2D myBody = null;
     // Use this for initialization
     void Start()
     {
+        LevelController.current.setStartPosition(transform.position);
         myBody = this.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update () {
-		
-	}
+        float value = Input.GetAxis("Horizontal");
+        Animator animator = GetComponent<Animator>();
+        
+        if (this.isGrounded)
+        {
+            animator.SetBool("jump", false);
+        }
+        else
+        {
+            animator.SetBool("jump", true);
+        }
+        if (Mathf.Abs(value) > 0&& this.isGrounded)
+        {
+            animator.SetBool("run", true);
+        }
+        else
+        {
+            animator.SetBool("run", false);
+        }
+    }
     void FixedUpdate()
     {
         float value = Input.GetAxis("Horizontal");
@@ -32,6 +56,44 @@ public class HeroController : MonoBehaviour {
         else if (value > 0)
         {
             sr.flipX = false;
+        }
+        Vector3 from = transform.position + Vector3.up * 0.3f;
+        Vector3 to = transform.position + Vector3.down * 0.1f;
+        int layer_id = 1 << LayerMask.NameToLayer("Ground");
+        
+        RaycastHit2D hit = Physics2D.Linecast(from, to, layer_id);
+        if (hit)
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+       
+        Debug.DrawLine(from, to, Color.red);
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            this.JumpActive = true;
+        }
+        if (this.JumpActive)
+        {
+            //Якщо кнопку ще тримають
+            if (Input.GetButton("Jump"))
+            {
+                this.JumpTime += Time.deltaTime;
+                if (this.JumpTime < this.MaxJumpTime)
+                {
+                    Vector2 vel = myBody.velocity;
+                    vel.y = JumpSpeed * (1.0f - JumpTime / MaxJumpTime);
+                    myBody.velocity = vel;
+                }
+            }
+            else
+            {
+                this.JumpActive = false;
+                this.JumpTime = 0;
+            }
         }
     }
 }
